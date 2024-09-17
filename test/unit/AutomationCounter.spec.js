9c639fc6cfd806adfca08b4615585dd1694c7d38
KeiroEmio <2077405059@qq.com>
2024-09-17 15:01:49 +0800

    PerformUpkeep 交易成功，Tx Hash: 0x27cdc28199269ac036852e392e5fcc25aa0b2d23022d3565f080c4d0a0445b16

--- test/unit/AutomationCounter.spec.js
+++ test/unit/AutomationCounter.spec.js
@@ -1,42 +1,156 @@
-const { network,ethers } = require("hardhat")
-const { loadFixture, time } = require("@nomicfoundation/hardhat-network-helpers")
+const { network } = require("hardhat")
+// const { loadFixture, time } = require("@nomicfoundation/hardhat-network-helpers")
 const { networkConfig,deployContractAddress, developmentChains } = require("../../helper-hardhat-config")
 const { assert, expect } = require("chai")
-// const {ethers} = require("ethers");
-
+const {ethers} = require("ethers");
+const AutomationCounterAddressABI = require("../../ABI/AutomationCounterAddressABI.json");
+require("dotenv").config()
+console.log("network.name:",developmentChains.includes(network.name))
 !developmentChains.includes(network.name)
     ? describe.skip
-    : describe("Automation Counter Unit Tests", async function () {
-          // We define a fixture to reuse the same setup in every test.
-          // We use loadFixture to run this setup once, snapshot that state,
-          // and reset Hardhat Network to that snapshot in every test.
-          async function deployAutomationCounterFixture() {
-              const [deployer] = await ethers.getSigners()
+    : describe("Automation Counter Unit Tests",  function () {
+        // We define a fixture to reuse the same setup in every test.
+        // We use loadFixture to run this setup once, snapshot that state,
+        // and reset Hardhat Network to that snapshot in every test.
+        // async function deployAutomationCounterFixture() {
+        const provider = new ethers.providers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
 
-              const chainId = network.config.chainId
-              console.log("chainId:",chainId)
-              // const automationUpdateInterval =
-              //     networkConfig[chainId]["automationUpdateInterval"] || "30"
-              //
-              // const counterFactory = await ethers.getContractFactory("AutomationCounter")
-              // const counter = await counterFactory
-              //     .connect(deployer)
-              //     .deploy(automationUpdateInterval)
-              // const AutomationCounterAddress = "0xc2Aff810D66ccD03b042B38ED8B75394355D09a1"
-              const AutomationCounterAddress =hre.config.deployContractAddress.AutomationCounterAddress;
-              // const AutomationCounterAddressABI = require("/ABI/AutomationCounterAddressABI.json")
-              // console.log("AutomationCounterAddressABI:",AutomationCounterAddressABI)
-              console.log("AutomationCounterAddress:",AutomationCounterAddress)
+        const privateKey = process.env.PRIVATE_KEY;
+        const wallet = new ethers.Wallet(privateKey, provider);
 
-              // const counter = new ethers.Contract(AutomationCounterAddress, AutomationCounterAddressABI, deployer);
-              const counter = await ethers.getContractAt("AutomationCounter", AutomationCounterAddress);
-              return { counter }
-          }
+        async function deployAutomationCounter() {
+
+            const AutomationCounterAddressABI = [
+                {
+                    "inputs": [
+                        {
+                            "internalType": "bytes",
+                            "name": "",
+                            "type": "bytes"
+                        }
+                    ],
+                    "name": "performUpkeep",
+                    "outputs": [],
+                    "stateMutability": "nonpayable",
+                    "type": "function"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "updateInterval",
+                            "type": "uint256"
+                        }
+                    ],
+                    "stateMutability": "nonpayable",
+                    "type": "constructor"
+                },
+                {
+                    "inputs": [
+                        {
+                            "internalType": "bytes",
+                            "name": "",
+                            "type": "bytes"
+                        }
+                    ],
+                    "name": "checkUpkeep",
+                    "outputs": [
+                        {
+                            "internalType": "bool",
+                            "name": "upkeepNeeded",
+                            "type": "bool"
+                        },
+                        {
+                            "internalType": "bytes",
+                            "name": "",
+                            "type": "bytes"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "counter",
+                    "outputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "",
+                            "type": "uint256"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "interval",
+                    "outputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "",
+                            "type": "uint256"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                },
+                {
+                    "inputs": [],
+                    "name": "lastTimeStamp",
+                    "outputs": [
+                        {
+                            "internalType": "uint256",
+                            "name": "",
+                            "type": "uint256"
+                        }
+                    ],
+                    "stateMutability": "view",
+                    "type": "function"
+                }
+            ]
+
+            console.log(hre.config.deployContractAddress.AutomationCounterAddress, AutomationCounterAddressABI, provider)
+            const counter = new ethers.Contract(hre.config.deployContractAddress.AutomationCounterAddress, AutomationCounterAddressABI, provider)
+            return {counter}
+        }
+
+        it("Example test", function () {
+            console.log("测试运行中...");
+            // 测试代码
+        });
+
+        describe("Automation Counter Unit Tests", function () {
+            describe("#performUpkeep", function () {
+                describe("success", function () {
+                    it("should be able to call performUpkeep after time passes", async function () {
+                        const { counter } = await deployAutomationCounter();
+                        const startingCount = await counter.counter();
+                        const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""));
+                        const interval = await counter.interval();
+                        console.log("interval:", interval);
+
+                        // 等待时间流逝
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+
+                        // 使用钱包签名调用 performUpkeep
+                        const tx = await counter.connect(wallet).performUpkeep(checkData);
+                        await tx.wait();
+
+                        console.log("PerformUpkeep 交易成功，Tx Hash:", tx.hash);
+                        console.log("计数器更新后:", (await counter.counter()).toNumber());
+
+                        // 断言计数器增加
+                        assert.equal(startingCount.toNumber() + 1, (await counter.counter()).toNumber());
+                    });
+                });
+            });
+        });
 
         describe("#checkUpkeep", async function () {
             describe("success", async function () {
                 it("should be able to call checkUpkeep", async function () {
-                    const { counter } = await deployAutomationCounterFixture()
+                    const { counter } = await deployAutomationCounter()
                     // const {counter } = await loadFixture(deployAutomationCounterFixture)
                     const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""))
                     const { upkeepNeeded } = await counter.callStatic.checkUpkeep(checkData)
@@ -46,38 +160,5 @@ const { assert, expect } = require("chai")
             })
         })
 
-          // describe("#checkUpkeep", async function () {
-          //     describe("success", async function () {
-          //         it("should be able to call checkUpkeep", async function () {
-          //             const { counter } = await loadFixture(deployAutomationCounterFixture)
-          //             const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""))
-          //             const { upkeepNeeded } = await counter.callStatic.checkUpkeep(checkData)
-          //             assert.equal(upkeepNeeded, false)
-          //         })
-          //     })
-          // })
-          //
-          // describe("#performUpkeep", async function () {
-          //     describe("success", async function () {
-          //         it("should be able to call performUpkeep after time passes", async function () {
-          //             const { counter } = await loadFixture(deployAutomationCounterFixture)
-          //             const startingCount = await counter.counter()
-          //             const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""))
-          //             const interval = await counter.interval()
-          //             await time.increase(interval.toNumber() + 1)
-          //             await counter.performUpkeep(checkData)
-          //             assert.equal(startingCount + 1, (await counter.counter()).toNumber())
-          //         })
-          //     })
-          //
-          //     describe("failure", async function () {
-          //         it("should not be able to call perform upkeep without the time passed interval", async function () {
-          //             const { counter } = await loadFixture(deployAutomationCounterFixture)
-          //             const checkData = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(""))
-          //             await expect(counter.performUpkeep(checkData)).to.be.revertedWith(
-          //                 "Time interval not met"
-          //             )
-          //         })
-          //     })
-          // })
-      })
+    })
+
